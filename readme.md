5dae7e0f0cea7030a8f585e859cc262d4b82fa8a
Piyush Srivastava <psrivastava@ghx.com>
2025-02-15 12:32:56 -0700

    todo

--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
 # Europa
 
 ## Goals
-[Publish a DomainEvent](https://courses.baeldung.com/courses/1295711/lectures/30604048)
+[Publish a DomainEvent-1](https://courses.baeldung.com/courses/1295711/lectures/30604048)
+[Publish a DomainEvent-2](https://medium.com/@padiahrohit/publishing-domain-events-spring-data-5075505f3f27)
+[Aggregates](https://www.baeldung.com/spring-data-ddd)
+[Publish to SQS](https://anand-guptaa.medium.com/event-driven-architecture-using-aws-sqs-and-spring-boot-d29fc3b1b25b)
+[SNS](https://docs.aws.amazon.com/AmazonS3/latest/userguide/ways-to-add-notification-config-to-bucket.html)
--- src/main/java/com/sppxs/europa/events/domain/listeners/PurchaseOrderCreatedListener.java
+++ src/main/java/com/sppxs/europa/events/domain/listeners/PurchaseOrderCreatedListener.java
@@ -2,15 +2,65 @@ package com.sppxs.europa.events.domain.listeners;
 
 import com.sppxs.europa.events.domain.PurchaseOrderCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class PurchaseOrderCreatedListener {
 
-    @EventListener
+    //@EventListener // -- method will get executed even if the save/update operation was rolled back
+    @TransactionalEventListener(classes = PurchaseOrderCreatedEvent.class) // -- will execute only if the transaction is committed
+    @Async
     public void handleMyDomainEvent(PurchaseOrderCreatedEvent event) {
         System.out.println("PurchaseOrderCreatedEvent - Guid: " + event.getPurchaseOrderGuid() + " | Amount: " + event.getAmount());
         //Asynchronously send an email to the user
         //Asynchronously send a SQS message notification
+        //What if the Publish event fails?? How to handle it??
+        // - Retry mechanism
+        // - Dead letter queue
+        // - Circuit breaker - Write to a log file
+        // - Error handling - Write to a log file
+        // - And a Job can run later to retry the failed events
+
+        /*
+        @EventListener
+        public void handleMyDomainEvent(MyDomainEvent event) {
+            SendMessageRequest sendMessageRequest = new SendMessageRequest()
+                    .withQueueUrl("your-sqs-queue-url")
+                    .withMessageBody(convertToJson(event));
+
+            amazonSQS.sendMessageAsync(sendMessageRequest, new SendMessageHandler());
+        }
+
+        private String convertToJson(Object event) {
+            // Implement JSON conversion logic here (e.g., using Jackson or Gson)
+            return "{\"entityId\":" + ((MyDomainEvent)event).getEntityId() + ", \"message\":\"" + ((MyDomainEvent)event).getMessage() + "\"}";
+        }
+
+        private static class SendMessageHandler implements AmazonSQSAsyncHandler<SendMessageRequest, SendMessageResult> {
+            @Override
+            public void onSuccess(SendMessageRequest request, SendMessageResult result) {
+                // Handle success (e.g., logging)
+                System.out.println("Message sent to SQS: " + result.getMessageId());
+            }
+
+            @Override
+            public void onError(Exception exception) {
+                // Handle error (e.g., logging, retry)
+                System.err.println("Error sending message to SQS: " + exception.getMessage());
+            }
+        }
+
+        @Configuration
+        public class SQSConfig {
+            @Bean
+            public AmazonSQSAsync amazonSQS() {
+                return AmazonSQSAsyncClientBuilder.defaultClient();
+            }
+        }
+         */
+
+
     }
 }
